013cbd16ff16f149c74270cd9f6c3428b09a4c3f
Nate Brady <nate@ultimus.cloud>
2024-04-10 13:51:11 -0500

    add debugging

--- ShiftLeftInlineApplication.py
+++ ShiftLeftInlineApplication.py
@@ -73,7 +73,7 @@ def perform_shift_left(args: List[str]):
             continue
 
         submit_response = json.loads(response["text"])
-        logging.info(response["text"])
+        logging.debug(f"Raw server response: {response["text"]}")
         status = submit_response["status"]
         message = submit_response["message"]
         if status.lower() == "failure":
